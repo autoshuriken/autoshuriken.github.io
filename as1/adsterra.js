ecb49035a210802818d8b1721bb0e9932b28245e
stupidbot <stupidbotpro@gmail.com>
2020-12-09 08:17:45 +0700

    Initial commit

---
+++ as1/adsterra.js
@@ -0,0 +1,9 @@
+//adsterra artybeezviralmailer
+atOptions = {
+	'key' : '3e966830b099bce74342deb4ad43b895',
+	'format' : 'iframe',
+	'height' : 250,
+	'width' : 300,
+	'params' : {}
+};
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://reciteassemble.com/3e966830b099bce74342deb4ad43b895/invoke.js"></scr' + 'ipt>');
